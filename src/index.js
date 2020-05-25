0e636223086c715b0ea9caec4bffbe476691f600
Gabriel Nunes Henke <gabrielchicao3@gmail.com>
2020-05-25 16:33:06 -0300

    Testando, agora vai

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const apolloServer = new ApolloServer({
 apolloServer.applyMiddleware({ app });
 
 app.get("/", (req, res) => {
-    res.send("Hello Agora Foi Meu Amigo :)");
+    res.send("Hello Agora Foi Meu Amigo :) xDxD");
 });
 
 app.listen(port);
