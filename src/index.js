ec7edb5aa98da7ef302d0a938efc1f01c5bea9db
Gabriel Nunes Henke <gabrielchicao3@gmail.com>
2020-05-27 18:25:30 -0300

    Porta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { userType } from "./graphql/User/UserType";
 import { userResolver } from "./graphql/User/UserResolver";
@@ -9,14 +8,13 @@ import { contactType } from "./graphql/Contact/ContactType";
 import { skillResolver } from "./graphql/Skill/SkillResolver";
 
 const app = express();
-app.use(cors)
+
 const apolloServer = new ApolloServer({
     typeDefs: [userType, queryType, skillType, contactType],
     resolvers: [userResolver, skillResolver],
 });
 
-const port = process.env.PORT || 4000;
 apolloServer.applyMiddleware({ app });
 app.get("/", (req, res) => res.send({ message: "API Skillus" }));
 
-app.listen(port);
+app.listen(8080);
