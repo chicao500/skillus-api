80a0ad4808d2710c82fe4e50d7a16ba4a933eefc
Gabriel Nunes Henke <gabrielchicao3@gmail.com>
2020-05-27 17:21:19 -0300

    Update cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { userType } from "./graphql/User/UserType";
 import { userResolver } from "./graphql/User/UserResolver";
@@ -8,13 +9,13 @@ import { contactType } from "./graphql/Contact/ContactType";
 import { skillResolver } from "./graphql/Skill/SkillResolver";
 
 const app = express();
-
+app.use(cors)
 const apolloServer = new ApolloServer({
     typeDefs: [userType, queryType, skillType, contactType],
     resolvers: [userResolver, skillResolver],
 });
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 apolloServer.applyMiddleware({ app, cors: false });
 
 app.listen(port);
