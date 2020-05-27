52378e2a3d20221ed2b3bd585ab976050c68575a
Gabriel Nunes Henke <gabrielchicao3@gmail.com>
2020-05-27 18:30:07 -0300

    .

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const apolloServer = new ApolloServer({
     resolvers: [userResolver, skillResolver],
 });
 
+const port = process.env.PORT || 8080;
 apolloServer.applyMiddleware({ app });
 app.get("/", (req, res) => res.send({ message: "API Skillus" }));
 
-app.listen(8080);
+app.listen(port);
